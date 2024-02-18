33bda55d739bb787b9bdfca4601fbc8b1e25e142
Elena Petrova <eop79@mail.ru>
2024-02-19 01:25:28 +0300

    v1

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -4,13 +4,13 @@ import java.net.Socket;
 public class Client {
     public static void main(String[] args) {
 
-        try(Socket clientSocket = new Socket("localhost", Server.PORT);
-            PrintWriter writer = new PrintWriter(clientSocket.getOutputStream(), true);
-            BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))){
+        try (Socket clientSocket = new Socket("localhost", Server.PORT);
+             PrintWriter writer = new PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
 
             writer.println("Elena");
             System.out.println(reader.readLine());
-        }catch (IOException e){
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -6,15 +6,15 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
-    public static  final Integer PORT = 8080;
+    public static final Integer PORT = 8080;
 
     public static void main(String[] args) {
-        try(ServerSocket serverSocket = new ServerSocket(PORT)){
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
             System.out.println("Сервер стартовал!");
-            while (true){
-                try(Socket clientSocket = serverSocket.accept();
-                    PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-                    BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));){
+            while (true) {
+                try (Socket clientSocket = serverSocket.accept();
+                     PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                     BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));) {
 
                     System.out.println("New connection accepted");
 
@@ -23,7 +23,7 @@ public class Server {
                     out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
                 }
             }
-        }catch (IOException e){
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
